c7d3bd065dc9cf879eb3419305ebf42417d77c8b
hmc-cs-hawk <gwomark@cs.hmc.edu>
2016-10-11 12:09:51 -0700

    Added scripts necessary for build. Made build.gradle contain modid and version. Added event handlers to mod file.

---
+++ src/main/java/com/gwomark/wommod/WomMod.java
@@ -0,0 +1,37 @@
+package com.gwomark.wommod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by gwomark on 10/11/16.
+*/
+
+@Mod(modid="wommod", name="Wom Mod", version="1.10.2-1.0")
+public class WomMod
+{
+    @Mod.instance("wommod")
+    public static WomMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public voide postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+
+}
